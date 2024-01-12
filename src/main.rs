59443b63925dcb2e1b0d6ae14235e4840d6267b4
zazu7765 <ohmyzazu@gmail.com>
2024-01-12 12:05:04 -0500

    feat: start prototype functions

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use clap::{Parser, Subcommand};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct CLI {
-    #[command(Subcommand)]
-    command: Commands,
+    #[command(subcommand)]
+    command: Option<Commands>,
+    #[arg(short, long)]
+    debug: bool,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
-    Add {},
-    List {},
-    Search {},
+    Add {
+        name: Vec<String>,
+        #[arg(short, long)]
+        tag: Option<String>,
+    },
+    List {
+        page: Option<u32>,
+    },
+    Search {
+        #[arg(short, long)]
+        date: Option<String>,
+        #[arg(short, long)]
+        tag: Option<String>,
+        content: Option<Vec<String>>,
+    },
     Edit {},
     Delete {},
 
@@ -19,11 +33,26 @@ enum Commands {
 
 fn main() {
     let cli = CLI::parse();
-    match cli.command {
-        Commands::Add {} => {}
-        Commands::List {} => {}
-        Commands::Search {} => {}
-        Commands::Edit {} => {}
-        Commands::Delete {} => {}
+    match &cli.command {
+        Some(Commands::Add { name, tag }) => {
+            let data = name.join(" ");
+            println!("{:?}", data);
+        }
+        Some(Commands::List { page }) => {
+            let page = page.unwrap_or_default();
+        }
+        Some(Commands::Search { date, tag, content }) => {
+            let date = date.as_deref().unwrap_or("");
+            let tag = tag.as_deref().unwrap_or("");
+            match (date.is_empty(), tag.is_empty()) {
+                (false, false) => println!("Filter by date and tag"),
+                (false, true) => println!("Filter by date NOT tag"),
+                (true, false) => println!("Filter by tag NOT date"),
+                _ => println!("Filter by content only"),
+            }
+        }
+        Some(Commands::Edit {}) => {}
+        Some(Commands::Delete {}) => {}
+        None => { println!("No Command Given!") }
     }
 }

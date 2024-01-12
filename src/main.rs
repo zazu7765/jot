7b6926f5ac4e67b365d62f8566f8ec7042c19555
zazu7765 <ohmyzazu@gmail.com>
2024-01-12 08:57:21 -0500

    feat: start building CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct CLI {
+    #[command(Subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Add {},
+    List {},
+    Search {},
+    Edit {},
+    Delete {},
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CLI::parse();
+    match cli.command {
+        Commands::Add {} => {}
+        Commands::List {} => {}
+        Commands::Search {} => {}
+        Commands::Edit {} => {}
+        Commands::Delete {} => {}
+    }
 }
